889071310799242411f6eb7924b303d4e9624476
nag <root@hyd.south.com>
2019-03-28 17:29:06 -0400

    myproject

---
+++ naresh/src/main/java/myvillage/App.java
@@ -0,0 +1,13 @@
+package myvillage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
